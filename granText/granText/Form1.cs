e7ca2d8d3a3f2b16ed630e84e6cc32e283649fc6
Alexander Malmstrom <jag@alexandermalmstrom.se>
2016-10-21 00:27:02 +0200

    Initial release

---
+++ granText/granText/Form1.cs
@@ -0,0 +1,43 @@
+﻿//Version 0.1
+//Date 2016-10-21
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace granText
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            mainTextBox.Text = String.Empty;
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
